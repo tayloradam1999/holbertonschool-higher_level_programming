b06431949dcd30ac6fa3dd38cadb3afe2a2b88e3
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-04-27 18:40:30 +0000

    Added abs function for negative ints

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    number *= -1
-if number % 10 < 6 and number % 10 > 0:
+number2 = number % 10
+if number < 0:
+    number2 = abs(number) % 10
+if number2 < 6 and number2 > 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, number % 10))
-elif number % 10 > 5:
+          .format(number, number2))
+elif number2 > 5:
     print("Last digit of {} is {} and is greater than 5"
-          .format(number, number % 10))
+          .format(number, number2))
 else:
-    print("Last digit of {} is {} and is 0".format(number, number % 10))
+    print("Last digit of {} is {} and is 0".format(number, number2))
