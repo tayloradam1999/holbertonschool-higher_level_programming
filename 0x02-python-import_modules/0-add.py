d250bd00d2081e3c015a31f7b4cf91e8565829f5
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 00:12:38 +0000

    Added name = main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}\n".format(a, b, add(a, b)), end='')
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}\n".format(a, b, add(a, b)), end='')
