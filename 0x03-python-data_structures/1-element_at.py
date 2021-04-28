01d5ee28d3d979ef9868b7eaa5bc71978141f05e
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 19:26:12 +0000

    Added >= to len of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return (None)
     a = my_list[idx]
     return (a)
