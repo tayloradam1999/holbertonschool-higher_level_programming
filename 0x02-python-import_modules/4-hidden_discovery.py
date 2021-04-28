dc9b610597503e953d675f269d4987d86aff19a4
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 01:46:47 +0000

    Pushing file for task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    dirlen = len(dir(hidden_4))
+    for i in range(0, dirlen):
+        names = dir(hidden_4)[i]
+        if names.startswith('__') == True:
+            continue
+        print("{}".format(names))
