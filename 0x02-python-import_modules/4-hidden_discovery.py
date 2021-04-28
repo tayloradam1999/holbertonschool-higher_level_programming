5c1c24d02e7a4875f5388ca9475f48bbcca2cd04
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 01:48:09 +0000

    Readded colon after if statement

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     dirlen = len(dir(hidden_4))
     for i in range(0, dirlen):
         names = dir(hidden_4)[i]
-        if names.startswith('__')
+        if names.startswith('__'):
             continue
         print("{}".format(names))
