b1195a5400c6eeb1d323f13f13d1aac8f6e619a7
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 01:54:14 +0000

    Changed main file name

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5 import a
-    print("{}".format(a))#!/usr/bin/python3
-    if __name__ == "__main__":
-        import variable_load_5 import a
-        print("{}".format(a))
+    from variable_load_5 import a
+    print("{}".format(a))
