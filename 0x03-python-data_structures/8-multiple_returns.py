311817746230c45e5e6b50333e2d8a957aa4d000
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 21:21:41 +0000

    Changed location of length = len(sentence)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
     if not sentence:
         char = None
     else:
-        length = len(sentence)
         char = sentence[:1]
     return (length, char)
