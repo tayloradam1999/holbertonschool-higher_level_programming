8ec6237a419c3e6701cf788fb9a307c22f7a7f25
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 21:39:02 +0000

    Added missing : after else, made file executable

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if not my_list:
+        return (None)
     results = []
     for i in my_list:
         if i % 2 == 0:
             results.append(True)
-        else
+        else:
             results.append(False)
     return (results)
