26266d1e016bb3980fd3ac456262adfcffe869b4
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-28 21:36:58 +0000

    Pushing file for task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    results = []
+    for i in my_list:
+        if i % 2 == 0:
+            results.append(True)
+        else
+            results.append(False)
+    return (results)
