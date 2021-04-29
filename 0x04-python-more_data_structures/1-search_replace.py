84c55ff213bc1069b692f85f59f443747ea7ab11
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-29 20:57:36 +0000

    Added i when searching for element to replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    new_list = [replace if == search else i for i in my_list]
+    new_list = [replace if i == search else i for i in my_list]
     return (new_list)
