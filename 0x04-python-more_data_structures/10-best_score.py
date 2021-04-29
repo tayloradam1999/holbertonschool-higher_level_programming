a557f74d16a71215f3fae2e918d43b9aaec4c0d0
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-29 23:54:32 +0000

    Pushing file for task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+            return None
+    v = list(a_dictionary.values())
+    k = list(a_dictionary.keys())
+    return k[v.index(max(v))]
