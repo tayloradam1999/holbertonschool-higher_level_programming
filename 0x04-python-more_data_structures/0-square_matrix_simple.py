82b407bbe62fb60c28eae8436b9deaf3f86b5de2
tayloradam1999 <tayloradam1999@gmail.com>
2021-04-29 20:37:03 +0000

    Fixed pep8 tab error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	return ([list(map(lambda x: x ** 2, array)) for array in matrix])
+    return ([list(map(lambda x: x ** 2, array)) for array in matrix])
