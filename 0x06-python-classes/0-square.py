2f3100477b5e9eb75f1638b87d6fff6a0a567125
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-04 22:37:20 +0000

    Reformatted documentation, took out def

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-
-"""
-mysquare - Write an empty class Square that defines a square.
-"""
+"""This Module creates a new, empty class called Square."""
 
 
-def mysquare():
-
-    """
-    mysquare - An empty class that defines a square.
-    """
-
-    class Square:
-        pass
-    mysquare = Square()
+class Square:
+    """Empty Class"""
+    pass
