8dc47926cbf51e4d9452ef39cd30b55f94c7c7a2
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-04 22:35:15 +0000

    Added class documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-
 """
 mysquare - Write an empty class Square that defines a square.
 """
 
 
 def mysquare():
+
+    """
+    mysquare - An empty class that defines a square.
+    """
+
     class Square:
         pass
     mysquare = Square()
