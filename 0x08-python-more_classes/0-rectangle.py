0611dd9306d55ffe384991f55323c4d477542c12
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-05 22:46:12 +0000

    Pushing file with documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+"""
+This module creates an empty class 'Rectangle' that
+defines a rectangle.
+"""
+
+
 class Rectangle:
+    """This is currently an empty class that defines a rectangle."""
     pass
