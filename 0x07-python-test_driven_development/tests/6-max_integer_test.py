87dfd702a01c9d35581778c76ab672a810ae01f2
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-06 03:15:37 +0000

    Pushing files for task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""This module is a unittest for 'max_integer(list=[]):'"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """This class runs tests for our function"""
+    def test_BegEnd(self):
+        """Tests max int on the beginning and end"""
+        self.assertEqual(max_integer([10, 2, 4, 5]), 10)
+        self.assertEqual(max_integer([2, 4, 5, 10]), 10)
+
+    def test_Inc(self):
+        """Tests for raising errors"""
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-10, -10, 10, 10]), 10)
+        self.assertEqual(max_integer([10, 10, 10, 10]), 10)
+        self.assertEqual(max_integer([1, 2, 2, 2]), 2)
+        self.assertRaises(TypeError, max_integer("max_integer"))
+        self.assertRaises(TypeError, max_integer((500, 5000)))
+        self.assertRaises(TypeError, max_integer([1.2, 1.3, 1.4, 1.5]))
+
+    def test_Mid(self):
+        """Tests for max int in the middle of a list"""
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
+        self.assertEqual(max_integer([1, 3, 2, 1]), 3)
