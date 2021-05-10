f36f3a1286be6be50281d879b5a86b84a944024f
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-10 03:17:06 +0000

    Pushing file with changed location of new test

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -9,12 +9,12 @@ def max_integer(list=[]):
     """
     if len(list) == 0:
         return None
+    if len(list) == 1:
+        result *= 2
     result = list[0]
     i = 1
     while i < len(list):
         if list[i] > result:
             result = list[i]
         i += 1
-    if len(list) == 1:
-        result *= 2
     return result
