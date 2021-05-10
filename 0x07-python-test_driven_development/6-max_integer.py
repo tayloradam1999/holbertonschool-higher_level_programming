bd6cc413e45ddde7c35356e11a02d2b8b8bd86ce
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-10 03:19:20 +0000

    Fixed compilation errors

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -9,9 +9,9 @@ def max_integer(list=[]):
     """
     if len(list) == 0:
         return None
+    result = list[0]
     if len(list) == 1:
         result *= 2
-    result = list[0]
     i = 1
     while i < len(list):
         if list[i] > result:
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,10 +17,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-10, -10, 10, 10]), 10)
         self.assertEqual(max_integer([10, 10, 10, 10]), 10)
         self.assertEqual(max_integer([1, 2, 2, 2]), 2)
+        self.assertEqual(max_integer([1]), 2)
         self.assertRaises(TypeError, max_integer("max_integer"))
         self.assertRaises(TypeError, max_integer((500, 5000)))
         self.assertRaises(TypeError, max_integer([1.2, 1.3, 1.4, 1.5]))
-        self.assertRaises(TypeError, max_integer([1.2]))
 
     def test_Mid(self):
         """Tests for max int in the middle of a list"""
