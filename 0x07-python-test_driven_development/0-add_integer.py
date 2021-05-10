daa8d58e7a0fa0ad7d67e9135e76eae40ad01de6
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-10 02:47:27 +0000

    Checking pep8 checker

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,11 +9,11 @@ Return: sum of addition
 
 
 def add_integer(a, b=98):
-	"""This def adds two integers and returns the sum.
-	Float arguments are typecasted to ints before additon is performed.
-	Raises a TypeError if either a or b is a non-integer or non-float."""
-	if ((not isinstance(a, int) and not isinstance(a, float))):
-		raise TypeError("a must be an integer")
-	if ((not isinstance(b, int) and not isinstance(b, float))):
-		raise TypeError("b must be an integer")
-	return (int(a) + int(b))
+    """This def adds two integers and returns the sum.
+    Float arguments are typecasted to ints before additon is performed.
+    Raises a TypeError if either a or b is a non-integer or non-float."""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
