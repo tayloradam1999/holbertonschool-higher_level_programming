f918bd4863610c6392ccd6b6a2da642952b8722d
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-11 23:27:35 +0000

    Changed to importing task 7 instead of having code in file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,23 +7,7 @@ Public instance method 'area(self)' that raises an exception with the message
 """
 
 
-class BaseGeometry:
-    """Empty class"""
-    pass
-
-    def area(self):
-        """Raises exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value
-        name is always a string
-        if value is not an integer, raises a TypeError
-        if value is less than or equal to 0, raises a ValueError"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
