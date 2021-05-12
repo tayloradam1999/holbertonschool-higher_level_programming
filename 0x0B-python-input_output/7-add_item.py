f53bc1d4ffe1e0940cab159db51714441391e025
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-12 22:43:06 +0000

    Pushing file for task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that adds all arguments to a
+list, then saves it to a file
+"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+
+except:
+    my_list = []
+
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[x])
+
+save_to_json_file(my_list, "add_item.json")
