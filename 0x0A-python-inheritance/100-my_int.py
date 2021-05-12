0a39ba3ca743eed501c6dc287c546579c787b128
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-12 00:20:02 +0000

    Pushing file for task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This modules writes a class 'MyInt' that inherits from 'int'
+"""
+
+
+class MyInt(int):
+    """-== and != operators are inverted"""
+    def __init__(self, integer):
+        self.integer = integer
+
+    def __eq__(self, other):
+        """Inverts == operator"""
+        if self.integer is other:
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+        """Inverts != operator"""
+        if self.integer is other:
+            return True
+        else:
+            return False
