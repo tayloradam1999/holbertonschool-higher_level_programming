dc11b37bca9b17dfd6540e4289ae019824f5ee0f
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-12 22:10:41 +0000

    Fixed pep8 error

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 """
 This module contains a function that returns the JSON
 representation of an object (string)
@@ -8,4 +7,5 @@ representation of an object (string)
 
 def to_json_string(my_obj):
     """Returns JSON rep. of an object"""
+    import json
     return json.dumps(my_obj)
