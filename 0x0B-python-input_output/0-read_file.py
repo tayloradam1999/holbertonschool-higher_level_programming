27aa4c9d267ab05cf81699e7859ba6b640c5ff81
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-12 21:45:36 +0000

    Added encoding=UTF-8 and end=

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ This module reads a text file and prints it to stdout
 
 def read_file(filename=""):
     """Reads a text file and prints to stdout"""
-    with open(filename) as fh:
-        print(fh.read())
+    with open(filename, encoding="UTF-8") as fh:
+        print(fh.read(), end="")
