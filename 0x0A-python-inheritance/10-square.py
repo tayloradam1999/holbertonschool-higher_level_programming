616b7bca7bbd9e675ca66f64ee5bd0243fc60997
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-12 00:05:25 +0000

    Swapped lines 18 and 19

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,8 +15,8 @@ class Square(Rectangle):
     'integer_validator'
     -area method must be implemented"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
