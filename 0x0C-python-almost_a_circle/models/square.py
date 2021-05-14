c43a2f80a93e6588337bd5c3aa5c681e7a2926c0
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-14 21:13:09 +0000

    Added public getter and setter size methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,9 +16,25 @@ class Square(Rectangle):
         """Constructor method that calls the super class
         This super call will use the logic of the __init__ of
         the 'Rectangle' class"""
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Returns our attributes values"""
         return '[Square] (%d) %d/%d - %d' % (self.id, self.x,
                                              self.y, self.height)
+
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter method for size"""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        elif size <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__size = size
