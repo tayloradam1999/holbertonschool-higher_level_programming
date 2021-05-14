1d43195970e8d67b7f2c10a931dfbd045609acd8
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-14 21:54:44 +0000

    Added update method to square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,13 +16,24 @@ class Square(Rectangle):
         """Constructor method that calls the super class
         This super call will use the logic of the __init__ of
         the 'Rectangle' class"""
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Returns our attributes values"""
         return '[Square] (%d) %d/%d - %d' % (self.id, self.x,
                                              self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        if args is None or len(args) == 0:
+            for i in kwargs:
+                if hasattr(self, i):
+                    setattr(self, i, kwargs[i])
+        largs = list(args)
+        latts = ["id", "size", "x", "y"]
+        for i in range(len(largs)):
+            setattr(self, latts[i], largs[i])
 
     @property
     def size(self):
