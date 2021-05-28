884554e84c0c8023d329ec0ad50cb3cd97b24741
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-28 00:54:16 +0000

    Added 'state_id INT NOT NULL line'

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,6 +8,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
     FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
