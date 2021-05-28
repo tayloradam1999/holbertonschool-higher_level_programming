0d5b1ed3b155f4e35c43afd48dcfbb79777cb71f
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-28 00:51:56 +0000

    Added database name in front of states when referencing prim key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,6 +8,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES cities(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
