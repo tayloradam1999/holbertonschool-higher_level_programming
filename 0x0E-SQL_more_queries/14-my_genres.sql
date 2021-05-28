a6981455d5b29e055616da8cb526a10f53c73c6b
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-28 02:15:47 +0000

    Pushing file for task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- The database name will be passed as an argument of the mysql command.
 SELECT name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
