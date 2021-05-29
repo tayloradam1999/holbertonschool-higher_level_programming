a26147b3f0c4385032d77e595eca3b269c1b74ea
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-29 19:16:59 +0000

    Fixed pep8 error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     from sys import argv
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}'".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}'"
+                   .format(argv[4]))
     data = cursor.fetchall()
     for i in data:
         print(i)
