a4cdd762bb3408d66910b209f9b38c290e5ea1e0
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-29 20:53:42 +0000

    Took out engine import

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ Class definition of a State and instance <Base = declarative_base()>
 """
 
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
