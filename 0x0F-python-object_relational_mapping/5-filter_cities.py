a52c31a614ba51d1229f1d902572813e28bb4fe8
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-29 20:09:02 +0000

    Pushing file for task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Lists all cities from the database
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    with db.cursor() as cursor:
+        cursor.execute("""
+            SELECT
+                cities.name
+            FROM
+                states
+            JOIN
+                cities
+            ON
+                states.id = cities.state_id
+            WHERE
+                states.name = BINARY %(states)s
+            """, {
+                'states': argv[4]
+            })
+        data = cursor.fetchall()
+    for i in range(len(data)):
+        if i != len(data) - 1:
+            print("{}, ".format(data[i][0]), end="")
+        else:
+            print(data[i][0])
