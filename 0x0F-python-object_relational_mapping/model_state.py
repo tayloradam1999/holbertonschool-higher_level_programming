39dc4dc535a282756c271b4fc61ddaaf4cb99930
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-29 20:48:47 +0000

    Took out session and argv imports

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,13 +7,9 @@ Class definition of a State and instance <Base = declarative_base()>
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sys import argv
 
 engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_6_usa',
                        echo=False)
-Session = sessionmaker(bind=engine)
-session = Session()
 Base = declarative_base()
 
 
