3c88fc050dacdd3483218f52e894cd74a0f1b547
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-03 20:23:34 +0000

    Added to what I had, still not finished

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
+let x = 0;
 if (myArgs.length === 0 || myArgs.length === 1) {
   console.log('%i', 0);
 } else {
-  let y = 0;
-  for (let x = 0; x < myArgs.length; x++) {
-    if (y < myArgs[x]) {
-      y = myArgs[x];
+  let largest = -2454635434;
+  for (x = 0; x < myArgs.length; x++) {
+    if (myArgs[x] > largest) {
+      largest = myArgs[x];
     }
   }
-  console.log(y);
+  let second = largest;
+  for (x = 0; x < myArgs.length; x++) {
+    if (myArgs[x] > second && myArgs[x] < largest) {
+      second = myArgs[x];
+    }
+  }
+  console.log(second);
 }
