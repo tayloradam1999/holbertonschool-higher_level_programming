e79c742eb30a8c675438c7d645b4ff47b9ab4283
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-03 21:55:25 +0000

    Pushing file for task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let myStr = '';
+    for (let x = 0; x < this.width; x++) {
+      myStr += 'X';
+    } for (let y = 0; y < myStr.length; y++) {
+      console.log(myStr);
+    }
+  }
+}
+module.exports = Rectangle;
