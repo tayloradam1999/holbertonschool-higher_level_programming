0c23de57611f52a7a325aa620e1ea3a9ba27c57b
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-03 00:38:29 +0000

    Pushing file for task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+myArgs = process.argv.slice(2);
+if (isNaN(myArgs[0])) {
+  console.log("%i", 1);
+} else {
+  console.log(factorial(myArgs[0]));
+}
+
+function factorial (x) {
+  return (x !== 1) ? x * factorial(x - 1) : 1;
+}
