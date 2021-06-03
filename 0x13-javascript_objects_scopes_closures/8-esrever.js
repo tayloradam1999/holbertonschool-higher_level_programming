001b8c9aa9f1f5648103a3166dcb2ee408df5f60
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-03 22:42:35 +0000

    Fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const myList = [];
-    for (let x = list.length - 1; x >= 0; x--) {
-        myList.push(list[x]);
-    }
-    return myList;
-}
+  const myList = [];
+  for (let x = list.length - 1; x >= 0; x--) {
+    myList.push(list[x]);
+  }
+  return myList;
+};
