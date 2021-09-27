3ca351e090649753bb0be4d68e690e51fd76cc53
Adam Taylor <tayloradam1999@gmail.com>
2021-09-27 21:22:47 +0000

    Pushing refactored task 5

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* adds item to list on click */
 
-$("DIV#add_item").click(function(){
-    $('UL.my_list').append('<li>Item</li>');
+$('#add_item').click(function () {
+	$('.my_list').append("<li>Item</li>");
 });
