f3ac29e518cf7717553228e45ea7d8bc438ad53b
Adam Taylor <tayloradam1999@gmail.com>
2021-09-27 21:31:31 +0000

    Pushing refactored task 6

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 /* Changes text of header on click */
 
-$("DIV#update_header").click(function(){
-    $('header').text('New Header!!!');
+// $("DIV#update_header").click(function(){
+//     $('header').text('New Header!!!');      --Working--
+// });
+
+$("#update_header").click(function () {
+	$("header").text("New Header!!!");
 });
