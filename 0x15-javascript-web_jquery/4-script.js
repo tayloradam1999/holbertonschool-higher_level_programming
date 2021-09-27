329b2f0715408eccc23e1a9d6ee11438bd648041
Adam Taylor <tayloradam1999@gmail.com>
2021-09-27 21:19:23 +0000

    Pushing refactored task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* toggles header element between red and green on click */
 
-$("DIV#toggle_header").click(function(){
-    $('header').toggleClass('red');
-    $('header').toggleClass('green');
+$('#toggle_header').click(function () {
+	$("header").toggleClass("red");
+	$("header").toggleClass("green");
 });
