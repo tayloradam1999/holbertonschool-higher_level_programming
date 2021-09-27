cbf33bd7cad0ccdf0a7ceb9d6d39d8842014fd85
Adam Taylor <tayloradam1999@gmail.com>
2021-09-27 21:06:38 +0000

    Pushing refactored task 2

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* changes header color to red on click */
 
-$("DIV#red_header").click(function(){
-    $('header').css('color', '#FF000');
+$('#red_header').click(function () {
+	$('header').css('color', '#FF0000');
 });
